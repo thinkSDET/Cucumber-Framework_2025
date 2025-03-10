0ee15162ecddd09df1aed9c835166f67543d98f8
Qa-Automation30 <qautomation32@gmail.com>
2025-03-10 15:21:03 +0530

    added couple of pages

--- src/test/java/baseClass/DriverManager.java
+++ src/test/java/baseClass/DriverManager.java
@@ -5,16 +5,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverManager {
 
+    public static ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
     public static WebDriver driver;
 
     public static WebDriver invokeDriver() {
         driver = new ChromeDriver();
+        driverThreadLocal.set(driver);
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         return driver;
     }
 
     public static WebDriver getDriver(){
-        return driver;
+        return driverThreadLocal.get();
     }
 }
