99e33ace6e406f4454ebd7924e180a33b6619058
Qa-Automation30 <qautomation32@gmail.com>
2025-03-11 10:45:50 +0530

    did change

--- src/test/java/utils/LoggerUtil.java
+++ src/test/java/utils/LoggerUtil.java
@@ -27,31 +27,4 @@ public class LoggerUtil {
         String methodName = stackTrace.getMethodName();
         return "[" + simpleClassName + "." + methodName + "] - ";
     }
-    /**
-     * I want to create a log file like this :
-     *
-     *   2025-03-10 19:01:14 [INFO]: ============================================================================
-     *   2025-03-10 19:01:14 [INFO]: Starting Test [Add one quantity from Store]
-     *   2025-03-10 19:01:14 [INFO]: Feature Name [Add to cart]
-     *   2025-03-10 19:01:14 [INFO]: ============================================================================
-     *   2025-03-10 19:01:14 [INFO]: --> Running Step: Launch the application with valid credentials
-     *    2025-03-10 19:01:14 [INFO]: - Initializing the browser :'chrome'
-     *
-     *    and so on
-     *
-     *    [Add one quantity from Store]  -> dynamic
-     *    [Add to cart] -> dynamic
-     *
-     * Feature: Add to cart
-     *   @Smoke
-     *   Scenario: Add one quantity from Store
-     *     * Launch the application with valid credentials
-     *     * Navigate to the store page
-     *     * Add the product from the store page
-     *       | Basic Blue Jeans |
-     *     * Navigate to cart page
-     *     * Verify that user can find the added product in cart page
-     *       | Product         | Quantity |
-     *       | Basic Blue Jeans | 1        |
-     */
 }
