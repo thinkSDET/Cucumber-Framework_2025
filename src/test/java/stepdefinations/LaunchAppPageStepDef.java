05f8e0d74de1712a08b5b6086665e82d3711df5e
Qa-Automation30 <qautomation32@gmail.com>
2025-03-11 18:24:13 +0530

    change

--- src/test/java/stepdefinations/LaunchAppPageStepDef.java
+++ src/test/java/stepdefinations/LaunchAppPageStepDef.java
@@ -5,7 +5,12 @@ import io.cucumber.java.en.Given;
 import pages.LaunchPage;
 
 public class LaunchAppPageStepDef {
-
+    /**
+     * In test automation, marking a Page Object (e.g., BillingDetailsPage) as private final ensures that its reference remains unchanged throughout the
+     * test execution, preventing accidental reassignment. While final helps maintain consistency within a single test execution,
+     * it does not make the object thread-safe. For parallel execution, ThreadLocal<T>
+     * should be used to ensure each test thread gets its own separate instance, avoiding concurrency issues when multiple tests run simultaneously.
+     */
     private final LaunchPage launchPage;
     public LaunchAppPageStepDef(PageObjectManager pageObjectManager) {
         this.launchPage = pageObjectManager.getLaunchPage();
