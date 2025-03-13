3d9a4d1e46a2bf3348badde425d3fc510080df47
Qa-Automation30 <qautomation32@gmail.com>
2025-03-13 11:21:50 +0530

    change

---
+++ src/test/java/stepDefinitions/MenPageStep.java
@@ -0,0 +1,22 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import pages.CheckOutPage;
+import pages.MenPage;
+import testBase.common.PageObjectManager;
+
+public class MenPageStep {
+
+    private final MenPage menPage;
+    public MenPageStep(PageObjectManager pageObjectManager){
+        this.menPage = pageObjectManager.getMePage();
+    }
+    @Given("Navigate to the Men page")
+    public void navigate_to_the_Men_page() {
+        menPage.navigateToMenPage();
+    }
+    @Given("Add the {string} from the Men page")
+    public void add_the_from_the_men_page(String product) {
+        menPage.addProducts(product);
+    }
+}
